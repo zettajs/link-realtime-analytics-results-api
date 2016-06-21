b1ca6ee36cf772c070b0a0327bc4a5ce710f7cb3
Matthew Dobson <mdobson4@gmail.com>
2016-06-21 19:53:30 -0400

    Fixes to the line parser.

--- influx_line_parser.js
+++ influx_line_parser.js
@@ -1,8 +1,9 @@
 module.exports = function(line) {
   var components = line.split(',');
 
-  if(components.length) {
+  if(components.length && components[0]) {
     var collection = components[0];
+    var aggregation = collection.split('.')[1];
     var deviceId = components[1].split('=')[1];
     var deviceType = components[2].split('=')[1];
     var hub = components[3].split('=')[1];
@@ -10,7 +11,7 @@ module.exports = function(line) {
     var deviceStream = averageComponents[0].split('=')[1];
     var value = averageComponents[1].split('=')[1];
     var timestamp = parseInt(averageComponents[2]) / 1000000;
-    var data = {hub: hub, topic: deviceType+'/'+deviceId+'/'+deviceStream, value: value, timestamp: timestamp};
+    var data = {hub: hub, topic: deviceType+'/'+deviceId+'/'+deviceStream, value: value, timestamp: timestamp, aggregation: aggregation};
     return {collection: collection, data: data};
   } else {
     return null;
